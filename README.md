a7810fb85efdef3340220fe8b03feff5618d7817
talivan51987 <talivan51987@gmail.com>
2020-02-26 17:48:24 -0400

    aumentando node

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # comando-bash
 archivos bash que tiene lineas de comandos de instalación de linux basado en debian y ubuntu
 cualquier comando que le pongan aumente su comentario o descripcion
+update user set authentication_string=PASSWORD("toor") where user='root';
--- comandos.sh
+++ comandos.sh
@@ -16,6 +16,24 @@ sudo apt-get install php libapache2-mod-php php-mysql -y
 sudo service apache2 restart
 sudo chown -R $USER:root /var/www/html
 sudo apt install phpmyadmin
+#instalar node
+sudo apt update
+sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt -y install nodejs
+sudo apt -y  install gcc g++ make
+wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
+    | sudo apt-key add -
 
+sudo apt update
+sudo apt install insomnia
+npm install -g @angular/cli
+#composer
+sudo su
+cd /usr/local/bin
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar composer
 
 
